2853da1ef086be83584b31b18befdcfdb2f703f7
eizn2003 <studizere@gmail.com>
2026-08-10 18:00:14 +0200

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {console.log(`server running on port ${PORT}`)};
