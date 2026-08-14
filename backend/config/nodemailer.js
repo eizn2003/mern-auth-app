ba46d18c5852d3241ca9dbb2b1d6fa1a7224065f
eizn2003 <studizere@gmail.com>
2026-08-14 12:28:55 +0200

    added temporaly SMTP debug

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -2,10 +2,19 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 export const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }    
+	host: process.env.SMTP_HOST,
+	port: process.env.SMTP_PORT,
+	secure: false,
+	auth: {
+		user: process.env.SMTP_USER,
+		pass: process.env.SMTP_PASS,
+	},
+});
+
+transporter.verify((error, success) => {
+	if (error) {
+		console.error("SMTP ERROR:", error);
+	} else {
+		console.log("SMTP SERVER READY:", success);
+	}
 });
